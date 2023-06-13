31ce85a3fd10cf0e6b108c60fba8f4e9f5cf0ffa
Eduarduar <eduardoarcega1997@gmail.com>
2023-06-12 21:28:14 -0600

    pagina publicaciones terminada

--- js/publicaciones.js
+++ js/publicaciones.js
@@ -1,3 +1,4 @@
+document.querySelector('a[href="./publicaciones"]').classList.add('active');
 // Obtener referencias a los elementos del DOM
 const container = document.querySelector('#modal-container'); // Contenedor del modal
 const input_pdf = document.querySelector('.pdf'); // Input de archivo PDF
@@ -5,6 +6,11 @@ const input_img = document.querySelector('.media'); // Input de archivo de image
 const btn_publicar = document.querySelector('#btn-publicar'); // Botón de publicar
 const publicador_text = document.querySelector('#publicador-text'); // Textarea del publicador
 const tipoPublicacion_select = document.querySelector('#tipo_publicacion');
+const img_view = document.querySelector('#img-view');
+const text_file = document.querySelector('#text-file');
+const text_pdf = document.querySelector('#text-pdf');
+const btn_cerrarModal = document.querySelector('#modal-publicar .modal-footer .btn-secondary');
+const modal_pdf = document.querySelector('#modal-pdf');
 
 // Función para redimensionar automáticamente el textarea (no se utiliza en el código proporcionado)
 function autoResize() {
@@ -36,22 +42,43 @@ function validateFileExtension(file, allowedExtensions) {
 }
 
 // Evento change del input de archivo PDF
-input_pdf.addEventListener('change', function () {
-  const file = this.files[0];
+input_pdf.addEventListener('change', function(e) {
+  const file = e.target.files[0];
   if (!validateFileExtension(file, ['pdf'])) {
     alert('Por favor, selecciona solo archivos PDF.');
-    this.value = '';
+    e.target.value = '';
   }
 });
 
+text_pdf.addEventListener('click', () => {
+  input_pdf.click();
+})
+text_file.addEventListener('click', () => {
+  input_img.click();
+});
+
 // Evento change del input de archivo de imagen
-input_img.addEventListener('change', function () {
-  const file = this.files[0];
-  if (!validateFileExtension(file, ['webp', 'png', 'jpg', 'jpeg'])) {
-    alert('Solo se aceptan los formatos webp, png, jpg y jpeg.');
-    this.value = '';
+input_img.addEventListener('change', function(e) {
+  const file = e.target.files[0];
+  try {
+    if (!validateFileExtension(file, ['webp', 'png', 'jpg', 'jpeg'])) {
+      alert('Solo se aceptan los formatos webp, png, jpg y jpeg.');
+      e.target.value = '';
+    }else if (e.target.files[0]){
+      const reader = new FileReader();
+      reader.onload = function(e){
+        img_view.src = e.target.result;
+      }
+      reader.readAsDataURL(e.target.files[0]);
+    }else{
+      img_view.src = '';
+    }  
+  } catch (error) {
+    img_view.src = '';
+    e.target.value = '';   
   }
 });
+
 
 // Evento click del botón de publicar
 btn_publicar.addEventListener('click', () => {
@@ -84,11 +111,14 @@ btn_publicar.addEventListener('click', () => {
       var decodificado = JSON.parse(respuesta);
       if (decodificado.code === '0') {
         // La operación en el servidor fue exitosa
-        publicador_text.value = '';
-        input_pdf.value = '';
-        input_img.value = '';
         document.querySelector('#modal-publicar .modal-footer .btn-secondary').click();
-        nuevaPublicacion(decodificado.datos)
+        nuevaPublicacion(decodificado.datos);
+        desactivarModalImg();
+        activarModalImg();
+        desactivarButtons_pdf();
+        activarButtons_pdf();
+        desactivarButtons_eliminar();
+        activarButtons_eliminar();
       } else {
         // La operación en el servidor no fue exitosa
         alert(decodificado.message);
@@ -133,6 +163,91 @@ const desactivarModalImg = function (){
     });
   });
 }
+const activarButtons_pdf = function() {
+  document.querySelectorAll('button[data-pdf]').forEach(el => {
+    el.addEventListener('click', function(e) {
+      e.stopPropagation();
+      document.querySelector('#pdfPreview').src = e.target.dataset.pdf;
+    })
+  })
+}
+const desactivarButtons_pdf = function() {
+  document.querySelectorAll('.icon-pdf').forEach(el => {
+    el.removeEventListener('click', function (e) {
+      e.stopPropagation();
+      document.querySelector('#pdfPreview').src = e.target.dataset.pdf;
+    })
+  })
+}
+
+const activarButtons_eliminar = function() {
+  document.querySelectorAll('.publicacion_eliminar').forEach(el => {
+    el.addEventListener('click', function(e) {
+      const formData = new FormData();
+      formData.append('eliminar', e.target.dataset.eliminar);
+      formData.append('id_user', id_user);
+      $.ajax({
+        url: '../db/consultas.php',
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (respuesta) {
+          var decodificado = JSON.parse(respuesta);
+          if (decodificado.code === '0') {
+            let padre = e.target.parentNode;
+            padre = padre.parentNode;
+            console.log(padre);
+            padre.parentNode.removeChild(padre);
+          } else {
+            // La operación en el servidor no fue exitosa
+            alert(decodificado.message);
+          }
+        },
+        error: function (error) {
+          // Manejar errores en la solicitud AJAX
+          console.log('Error en la solicitud AJAX:', error);
+        }
+      });
+    })
+  })
+}
+const desactivarButtons_eliminar = function() {
+  document.querySelectorAll('.publicacion_eliminar').forEach(el => {
+    el.removeEventListener('click', function(e) {
+      const formData = new FormData();
+      formData.append('eliminar', e.target.dataset.eliminar);
+      formData.append('id_user', id_user);
+      $.ajax({
+        url: '../db/consultas.php',
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (respuesta) {
+          var decodificado = JSON.parse(respuesta);
+          if (decodificado.code === '0') {
+            let padre = e.target.parentNode;
+            do {
+              padre = padre.parentNode;
+              console.log(padre);
+            } while (padre.classList.contains('container-publicacion'));
+            padre.parentNode.removeChild(padre);
+          } else {
+            // La operación en el servidor no fue exitosa
+            alert(decodificado.message);
+          }
+        },
+        error: function (error) {
+          // Manejar errores en la solicitud AJAX
+          console.log('Error en la solicitud AJAX:', error);
+        }
+      });
+    })
+  })
+}
+activarButtons_pdf()
+activarButtons_eliminar();
 
 // funcion que crea la estructura de una nueva publicacion con los datos que se le proporcionan
 const nuevaPublicacion = function (datos){
@@ -179,10 +294,15 @@ const nuevaPublicacion = function (datos){
     const pdf = document.createElement('div');
     pdf.className = 'publicacion_pdf';
     const pdfButton = document.createElement('button');
+    pdfButton.setAttribute('data-bs-toggle', 'modal');
+    pdfButton.setAttribute('data-bs-target', '#modal-pdf');
+    pdfButton.setAttribute('data-pdf', `${datos.pdf}`);
     const pdfIcon = document.createElement('span');
+    pdfIcon.setAttribute('data-pdf', `${datos.pdf}`);
     pdfIcon.className = 'icon-pdf material-symbols-outlined';
     pdfIcon.textContent = 'picture_as_pdf';
     const pdfText = document.createElement('span');
+    pdfText.setAttribute('data-pdf', `${datos.pdf}`);
     pdfText.textContent = 'ver pdf adjunto';
     pdfButton.appendChild(pdfIcon);
     pdfButton.appendChild(pdfText);
@@ -198,15 +318,19 @@ const nuevaPublicacion = function (datos){
   eliminar.className = 'publicacion_eliminar fa fa-times';
   eliminar.setAttribute('aria-hidden', 'true');
   eliminar.textContent = ' Eliminar';
-  eliminar.setAttribute('id',`${datos.publicacion}`);
+  eliminar.setAttribute('data-eliminar',`${datos.publicacion}`);
   footer.appendChild(eliminar);
 
   publicacion.appendChild(footer);
 
   containerPublicaciones.prepend(publicacion);
 
-  desactivarModalImg()
-  activarModalImg()
-
 }
+
+btn_cerrarModal.addEventListener('click', () => {
+  publicador_text.value = '';
+  input_pdf.value = '';
+  input_img.value = '';
+  img_view.src = ''; 
+});
 
